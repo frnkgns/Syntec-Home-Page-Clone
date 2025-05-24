de40e4037ee500c33b090cc7e5d80d657e730e2f
Frank Gines <frnk.dvlpr@gmail.com>
2025-05-25 01:32:33 +0800

    removing . to ./assets

--- assets/js/allgames.js
+++ assets/js/allgames.js
@@ -1,19 +1,19 @@
 const GamesImage = [
-  "./assets/images/allgames/angrybirds.png",
-  "./assets/images/allgames/apex.png",
-  "./assets/images/allgames/clasofclans.png",
-  "./assets/images/allgames/cod.png",
-  "./assets/images/allgames/eldenofrings.png",
-  "./assets/images/allgames/fc2024.png",
-  "./assets/images/allgames/fortnite.png",
-  "./assets/images/allgames/godofwar.png",
-  "./assets/images/allgames/leagueoflegends.png",
-  "./assets/images/allgames/minecraft.png",
-  "./assets/images/allgames/mortalcombat.png",
-  "./assets/images/allgames/rogue.png",
-  "./assets/images/allgames/valorant.png",
-  "./assets/images/allgames/warzone.png",
-  "./assets/images/allgames/zelda.png"
+  "/assets/images/allgames/angrybirds.png",
+  "/assets/images/allgames/apex.png",
+  "/assets/images/allgames/clasofclans.png",
+  "/assets/images/allgames/cod.png",
+  "/assets/images/allgames/eldenofrings.png",
+  "/assets/images/allgames/fc2024.png",
+  "/assets/images/allgames/fortnite.png",
+  "/assets/images/allgames/godofwar.png",
+  "/assets/images/allgames/leagueoflegends.png",
+  "/assets/images/allgames/minecraft.png",
+  "/assets/images/allgames/mortalcombat.png",
+  "/assets/images/allgames/rogue.png",
+  "/assets/images/allgames/valorant.png",
+  "/assets/images/allgames/warzone.png",
+  "/assets/images/allgames/zelda.png"
 ];
 
 const GameImageContainer = document.getElementById("gameImageContainer");
--- assets/js/wager.js
+++ assets/js/wager.js
@@ -2,19 +2,19 @@
 // put you assets images in the following directories:
 // then change this code to match your images paths
 const wagerLeft = [
-  "./assets/images/wager/left/battle-1.png",
-  "./assets/images/wager/left/battle-3.png",
-  "./assets/images/wager/left/battle-5.png",
-  "./assets/images/wager/left/battle-7.png",
-  "./assets/images/wager/left/battle-9.png"
+  "/assets/images/wager/left/battle-1.png",
+  "/assets/images/wager/left/battle-3.png",
+  "/assets/images/wager/left/battle-5.png",
+  "/assets/images/wager/left/battle-7.png",
+  "/assets/images/wager/left/battle-9.png"
 ];
 
 const wagerRight = [
-  "./assets/images/wager/right/battle-2.png",
-  "./assets/images/wager/right/battle-4.png",
-  "./assets/images/wager/right/battle-6.png",
-  "./assets/images/wager/right/battle-8.png",
-  "./assets/images/wager/right/battle-10.png"
+  "/assets/images/wager/right/battle-2.png",
+  "/assets/images/wager/right/battle-4.png",
+  "/assets/images/wager/right/battle-6.png",
+  "/assets/images/wager/right/battle-8.png",
+  "/assets/images/wager/right/battle-10.png"
 ];
 
 let index = 0;
